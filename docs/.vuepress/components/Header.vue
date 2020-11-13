8218f1d7da42c82c62eaa595bc49468569195fdb
sven <sven@so36.net>
2020-11-13 15:50:29 +0100

    vuepress(components): add header component

---
+++ docs/.vuepress/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{$page.frontmatter.title}}</h1>
+    <div
+      style="width: 4rem; height: .4rem; margin: .1rem 0rem 2rem .3rem; background-color: #5469d4;"
+    ></div>
+    <p>{{$page.frontmatter.description}}</p>
+    <br />
+    <!-- &#x1F4A1; {{ $page.readingTime.text }} -->
+
+  </div>
+</template>
